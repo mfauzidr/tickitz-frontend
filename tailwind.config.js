0670140975b1415bd11e64b4426f9f611859841b
Khalif Gaffarezka <kgaffarezka@gmail.com>
2024-08-22 20:46:33 +0700

    first setup

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+const tailwindConfig = {
+  content: ["./src/**/*.tsx"],
+  theme: {
+    screens: {
+      xxxsm: "300px",
+      xxsm: "321px",
+      xsm: "376px",
+      tbt: "426px",
+      sm: "641px",
+      md: "769px",
+      lg: "1025px",
+      xl: "1281px",
+      "2xl": "1441px",
+      "4xl": "1537px",
+      uw: "2041px",
+    },
+    extend: {
+      fontFamily: {
+        mulish: ["Mulish", "sans-serif"],
+      },
+      colors: {
+        primary: "#1D4ED8",
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default tailwindConfig;
