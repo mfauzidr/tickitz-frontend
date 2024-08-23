04d6d4cc8b12acae45dcf1ab76721fdf34d578e8
Khalif Gaffarezka <kgaffarezka@gmail.com>
2024-08-23 15:17:06 +0700

    add auth page

---
+++ src/components/Input.tsx
@@ -0,0 +1,33 @@
+import { ChangeEventHandler } from "react";
+
+type InputProps = {
+  input: {
+    onChange?: ChangeEventHandler<HTMLInputElement>;
+    type?: string;
+    name?: string;
+    placeholder?: string;
+    autocomplete?: string;
+    value?: string;
+  };
+  isToggler?: boolean;
+  onTogglerHandler?: (e: React.MouseEvent) => void;
+};
+
+function Input(props: InputProps) {
+  const { input } = props;
+
+  return (
+    <input
+      className="w-full h-10 border border-solid border-gray-200 rounded-lg pl-4 mb-3 text-gray-300 text-xs"
+      type={input.type}
+      id={input.name}
+      name={input.name}
+      value={input.value}
+      placeholder={input.placeholder}
+      autoComplete={input.autocomplete}
+      onChange={input.onChange}
+    />
+  );
+}
+
+export default Input;
--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Desktop";
 import Index from "./pages/Index";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function Error() {
   return <div>Error</div>;
@@ -20,99 +22,85 @@ const routerWithChildren = createBrowserRouter([
         path: "home",
         element: <Home />,
       },
-    //   {
-    //     path: "product",
-    //     element: <Product />
-    //   },
-    //   {
-    //     path: "checkoutproduct",
-    //     element: (
-    //       <PrivateRoute to={"/login"}>
-    //         <CheckoutProduct />
-    //       </PrivateRoute>
-    //     ),
-    //   },
-    //   {
-    //     path: 'orderdetails',
-    //     element: (
-    //       <PrivateRoute to={"/login"}>
-    //         <OrderDetails />
-    //       </PrivateRoute>
-    //     ),
-    //   },
-    //   {
-    //     path: 'detailsproduct',
-    //     element: (
-    //       <PrivateRoute to={"/login"}>
-    //         <DetailProduct />
-    //       </PrivateRoute>
-    //     ),
-    //   },
-    //   {
-    //     path: '/detailsproduct/:id',
-    //     element: (
-    //       <PrivateRoute to={"/login"}>
-    //         <DetailProduct />
-    //       </PrivateRoute>
-    //     ),
-    //   },
-    //   {
-    //     path: '/ordersdetail/:id',
-    //     element: (
-    //       <PrivateRoute to={"/login"}>
-    //         <OrderDetails />
-    //       </PrivateRoute>
-    //     ),
-    //   }
-    //   ,
-    //   {
-    //     path: 'historyorder',
-    //     element: (
-    //       <PrivateRoute to={"/login"}>
-    //         <HistoryOrder />
-    //       </PrivateRoute>
-    //     ),
-    //   },
-    //   {
-    //     path: 'profile',
-    //     element: (
-    //       <PrivateRoute to={"/login"}>
-    //         <Profile />
-    //       </PrivateRoute>
-    //     ),
-    //   },
-    //   {
-    //     path: 'axios',
-    //     element: <AxiosTry/>
-    //   },
-      {
-        path: "*",
-        element: <NotFound />,
-      },
+      //   {
+      //     path: "product",
+      //     element: <Product />
+      //   },
+      //   {
+      //     path: "checkoutproduct",
+      //     element: (
+      //       <PrivateRoute to={"/login"}>
+      //         <CheckoutProduct />
+      //       </PrivateRoute>
+      //     ),
+      //   },
+      //   {
+      //     path: 'orderdetails',
+      //     element: (
+      //       <PrivateRoute to={"/login"}>
+      //         <OrderDetails />
+      //       </PrivateRoute>
+      //     ),
+      //   },
+      //   {
+      //     path: 'detailsproduct',
+      //     element: (
+      //       <PrivateRoute to={"/login"}>
+      //         <DetailProduct />
+      //       </PrivateRoute>
+      //     ),
+      //   },
+      //   {
+      //     path: '/detailsproduct/:id',
+      //     element: (
+      //       <PrivateRoute to={"/login"}>
+      //         <DetailProduct />
+      //       </PrivateRoute>
+      //     ),
+      //   },
+      //   {
+      //     path: '/ordersdetail/:id',
+      //     element: (
+      //       <PrivateRoute to={"/login"}>
+      //         <OrderDetails />
+      //       </PrivateRoute>
+      //     ),
+      //   }
+      //   ,
+      //   {
+      //     path: 'historyorder',
+      //     element: (
+      //       <PrivateRoute to={"/login"}>
+      //         <HistoryOrder />
+      //       </PrivateRoute>
+      //     ),
+      //   },
+      //   {
+      //     path: 'profile',
+      //     element: (
+      //       <PrivateRoute to={"/login"}>
+      //         <Profile />
+      //       </PrivateRoute>
+      //     ),
+      //   },
+      //   {
+      //     path: 'axios',
+      //     element: <AxiosTry/>
+      //   },
     ],
-  }
-//   {
-//     path: '/login',
-//     element: <Login/>,
-//   },
-//   {
-//     path: '/register',
-//     element: <Register/>,
-//   },
-//   {
-//     path: '/forgot',
-//     element: <ForgotPass/>
-//   },
-//   {
-//     path: "*",
-//     element: <NotFound />,
-//   },
-//   {
-//     path: "/admin-toko-kopi",
-//     element: <AdminDashboard />,
-//     errorElement: <Error />,
-//     children: []
-//   }
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
 ]);
 
 export default routerWithChildren;
