89e431efc18abf6340811614890212c08e4aa751
Khalif Gaffarezka <kgaffarezka@gmail.com>
2024-08-26 10:50:53 +0700

    khalif edit movie details

--- src/components/MovieBannerDetails.tsx
+++ src/components/MovieBannerDetails.tsx
@@ -24,11 +24,11 @@ const MovieBannerAndDetails: React.FC<MovieBannerAndDetailsProps> = ({ movie }) 
         <div className="flex relative w-full rounded-md bg-black bg-opacity-40 min-h-[415px] max-md:max-w-full" />
       </div>
       <div className="relative z-[3] -mt-64 md:-mt-44">
-        <div className="px-4 tbt:px-10 lg:px-32 md:flex md:gap-5">
+        <div className="px-4 tbt:px-10 lg:px-32 md:flex md:gap-5 md:mt-5">
           <div className="grid place-items-center">
             <img loading="lazy" width="250" src={movie.poster} alt="Movie poster" className="rounded-md" />
           </div>
-          <div className="mt-10 md:mt-44">
+          <div className="mt-10 md:mt-0 md:self-end">
             <h1 className="text-2xl text-center md:text-start font-bold">{movie.title}</h1>
             <div className="flex gap-2 justify-center md:justify-normal mt-6 text-center text-gray-400">
               {movie.genres.map((genre, index) => (
